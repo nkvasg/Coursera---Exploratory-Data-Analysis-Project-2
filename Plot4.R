48ddc6f1144985d6300c5cc9cae9aa7ba4ffa7a1
nkvasg <nkvasg@yahoo.com>
2015-03-13 09:37:51 +1000

    Create Plot4.R

---
+++ Plot4.R
@@ -0,0 +1,15 @@
+source("load_data.R")
+head(SCC$Short.Name)
+
+png(filename = "plot4.png", width = 480, height = 480, units = "px")
+coal <- grep("coal", SCC$Short.Name, ignore.case = T)
+coal <- SCC[coal, ]
+coal <- NEI[NEI$SCC %in% coal$SCC, ]
+
+coalEmissions <- aggregate(coal$Emissions, list(coal$year), sum)
+
+plot(coalEmissions, type = "l", xlab = "Year", 
+     main = "Total Emissions From Coal Combustion-related\n Sources from 1999 to 2008", 
+     ylab = expression('Total PM'[2.5]*" Emission"))
+
+dev.off()
